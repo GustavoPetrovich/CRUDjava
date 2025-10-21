2b09a6c731e790dbc87b4be230da14619dee90ad
GustavoPetrovich <gutorpetrovich@gmail.com>
2025-10-20 22:01:07 -0300

    Criação da classe Conexao
    Criação da classe Conexao para estabelecer conexao ao banco de dados MySQL

--- src/main/java/Janelas/CadastroProduto.java
+++ src/main/java/Janelas/CadastroProduto.java
@@ -4,6 +4,8 @@
  */
 package Janelas;
 
+import BD.Conexao;
+
 /**
  *
  * @author gustavo.rpetrovich
@@ -15,6 +17,12 @@ public class CadastroProduto extends javax.swing.JFrame {
      */
     public CadastroProduto() {
         initComponents();
+        
+        if (Conexao.getConnection() != null){
+            System.out.println("Conexão Realizada");
+        } else {
+            System.out.println("Falha na Conexão");
+        }
     }
 
     /**
